411d0f67f60f2434fe3d68a9e7543b4d7816466f
Patrick Lehmann <patlehmann1@gmail.com>
2018-07-22 12:55:20 -0400

    getting somewhere

---
+++ index.js
@@ -0,0 +1,6 @@
+var Word = require("./word.js");
+
+var wordArray = ["HTML", "CSS", "JAVASCRIPT", "JQUERY", "NODEJS", "MYSQL", "API", "EXPRESSJS", "JAVA", "GITHUB", "GIT",
+"HEROKU", "LINUX", "PYTHON", "REACT", "ANGULAR", "FIREBASE"];
+
+var newWordToStart = wordArray[Math.floor(Math.random() * wordArray.length)];
--- word.js
+++ word.js
@@ -1,11 +1,17 @@
 var Letter = require("./letter");
 
-var Word = function (word){
+var Word = function (word) {
     this.wordString = word;
-    this.wordArray=wordString.split("");
-    this.returnWord = function (){
-        for (i = 0; i < wordArray.length; i++){
-            this.wordArray.push(letter.displayLetter(wordArray[i]);
+    this.newWord = [];
+    this.wordArray = wordString.split("");
+    this.returnWord = function () {
+        for (i = 0; i < wordArray.length; i++) {
+            this.newWord.push(letter.displayLetter(wordArray[i]));
         }
     }
+    this.wordGuess = function(guess){
+        this.checkLetter(guess);
+    }
 }
+
+module.exports = Word;
