0d7d6abf8bb57fd0d1ef321538e6f4fe7e6f9e20
Patrick Lehmann <patlehmann1@gmail.com>
2018-07-22 00:00:47 -0400

    this is what I have so far

--- word.js
+++ word.js
@@ -1,11 +1,11 @@
 var Letter = require("./letter");
 
 var Word = function (word){
-    this.objectArray = [];
-    this.blanks = [];
+    this.wordString = word;
+    this.wordArray=wordString.split("");
     this.returnWord = function (){
-        for (i = 0; i < words.length; i++){
-            this.letter
+        for (i = 0; i < wordArray.length; i++){
+            this.wordArray.push(letter.displayLetter(wordArray[i]);
         }
     }
 }
